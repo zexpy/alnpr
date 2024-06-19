58a24eb6d99da941b0f6f3b1512fe25e49cd8b26
morphhyy <imbibek05@gmail.com>
2024-06-19 22:10:53 +0545

    Configure the tailwind css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,34 +4,43 @@ import "./App.css";
 const URL = "http://localhost:8000/";
 
 const App = () => {
-    const [image, setImage] = useState<string>();
-    const [file, setFile] = useState<File>();
+  const [image, setImage] = useState<{ full: string; crop: string }>();
+  const [file, setFile] = useState<File>();
 
-    const handleUpload = async () => {
-        const formData = new FormData();
-        formData.append("file", file as Blob);
-        const response = await fetch("http://localhost:8000/image", {
-            method: "POST",
-            body: formData,
-        });
-        const data: { url: string; path: string } = await response.json();
-        setImage(`${URL}${data.url}/${data.path}`);
-    };
+  const handleUpload = async () => {
+    const formData = new FormData();
+    formData.append("file", file as Blob);
+    const response = await fetch("http://localhost:8000/image", {
+      method: "POST",
+      body: formData,
+    });
+    const data: { url: string; path: string } = await response.json();
+    setImage({
+      full: `${URL}${data.url}/${data.path}`,
+      crop: `${URL}${data.url}/crops/number_plate/${data.path}`,
+    });
+  };
 
-    return (
+  return (
+    <>
+      <div>
+        <input type="file" onChange={(e) => setFile(e.target?.files?.[0])} />
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+      {image && (
         <>
-            <div>
-                <input type="file" onChange={(e) => setFile(e.target?.files?.[0])} />
-                <button onClick={handleUpload}>Upload</button>
-            </div>
-            {image && (
-                <img
-                    src={image}
-                    style={{ width: 700, height: 700, objectFit: "contain" }}
-                />
-            )}
+          <img
+            src={image.full}
+            style={{ width: 700, height: 700, objectFit: "contain" }}
+          />
+          <img
+            src={image.crop}
+            style={{ width: 700, height: 700, objectFit: "contain" }}
+          />
         </>
-    );
+      )}
+    </>
+  );
 };
 
 export default App;
