87648833abfc2ff1b8d282df4c9cc5cd968f6887
morphhyy <imbibek05@gmail.com>
2024-06-20 19:18:08 +0545

    Change the name configuration

--- backend/detect.py
+++ backend/detect.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('/home/zephyr/Desktop/.code/college/alpr/results/yolov82/weights/best.pt')
+model = YOLO('/home/zephyr/Desktop/.code/college/alnpr/results/yolov82/weights/best.pt')
 
 
 res = model.predict('/home/zephyr/Downloads/ss.jpg', project='result', name='yolo', save=True, exist_ok=True)
